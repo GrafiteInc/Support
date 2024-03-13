55dcbd5b59e87fec28b7d6480b79e77dc180f288
Matt Lantz <mattlantz@gmail.com>
2024-03-13 14:45:45 -0400

    because of docs

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/up', function () {
     return view('support::up');
 });
+
+if (! app()->environment(['staging', 'production'])) {
+    Route::get('/docs', function () {
+        $files = scandir(base_path('docs'));
+
+        foreach ($files as $file) {
+            if (! is_dir(base_path('docs/'.$file))) {
+                $sections[$file] = file_get_contents(base_path('docs/'.$file));
+            }
+        }
+
+        return view('support::docs', [
+            'sections' => $sections,
+        ]);
+    });
+}
---
+++ views/docs.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('page-title', 'Application Docs')
+
+@section ('app-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="mt-5">
+                    <h4 class="mb-0">
+                        <span class="fa fa-book text-success fa-fw"></span>
+                        Docs
+                    </h4>
+                    <div class="list-group mt-5">
+                        @foreach ($sections as $section => $content)
+                            <a href="#{{ Str::of($section)->snake()->replace('.md', '') }}" class="list-group-item list-group-item-action">{{ Str::of($section)->title()->replace('.md', '') }}</a>
+                        @endforeach
+                        <a href="/docs/api" class="list-group-item list-group-item-action">API Docs</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="mt-5">
+                    @foreach ($sections as $section => $content)
+                        <div id="{{ Str::of($section)->snake()->replace('.md', '') }}" class="card shadow-sm mb-5">
+                            <div class="card-header">
+                                {{ Str::of($section)->title()->replace('.md', '') }}
+                            </div>
+                            <div class="card-body">
+                                {!! Str::of($content)->markdown() !!}
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
